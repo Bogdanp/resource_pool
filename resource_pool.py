df22dd2d0be6b36bbecb1efbc151894353c12323
Bogdan Popa <popa.bogdanp@gmail.com>
2018-09-18 19:43:19 +0300

    feat: add LazyPool

--- resource_pool.py
+++ resource_pool.py
@@ -2,9 +2,10 @@ import typing
 
 from contextlib import contextmanager
 from queue import Queue, Empty, Full
-from typing import Generator, Generic, Optional
+from typing import Generator, Generic, Optional, List
+from threading import Condition
 
-__all__ = ["PoolError", "PoolTimeout", "PoolFull", "Pool", "__version__"]
+__all__ = ["PoolError", "PoolTimeout", "PoolFull", "Pool", "LazyPool", "__version__"]
 __version__ = "0.1.2"
 
 ResourceT = typing.TypeVar("ResourceT")
@@ -101,3 +102,103 @@ class Pool(Generic[ResourceT]):
         """Get the number of resources currently in the pool.
         """
         return self._pool.qsize()
+
+
+class LazyPool(Generic[ResourceT]):
+    """A generic resource pool that lazily creates resources.
+
+    Parameters:
+      factory: The factory function that is used to create resources.
+      pool_size: The max number of resources in the pool at any time.
+    """
+
+    _factory: ResourceFactory
+    _cond: Condition
+    _pool: List[ResourceT]
+    _pool_size: int
+    _used_size: int
+
+    def __init__(self, factory: ResourceFactory, *, pool_size: int, min_instances: int = 0) -> None:
+        assert pool_size > min_instances, "pool_size must be larger than min_instances"
+
+        self._factory = factory
+        self._cond = Condition()
+        self._pool = []
+        self._pool_size = pool_size
+        self._used_size = 0
+
+        for _ in range(min_instances):
+            self._used_size += 1
+            self.put(factory())
+
+    @contextmanager
+    def reserve(self, timeout: Optional[float] = None) -> Generator[ResourceT, None, None]:
+        """Reserve a resource and then put it back.
+
+        Example:
+          with pool.reserve(timeout=10) as res:
+            print(res)
+
+        Raises:
+          Timeout: If a timeout is given and it expires.
+
+        Parameters:
+          timeout: An optional timeout representing how long to wait
+            for the resource.
+
+        Returns:
+          A resource.
+        """
+        resource = self.get(timeout=timeout)
+        try:
+            yield resource
+        finally:
+            self.put(resource)
+
+    def get(self, *, timeout: Optional[float] = None) -> ResourceT:
+        """Get a resource from the pool.
+
+        It's the getter's responsibility to put the resource back once
+        they're done using it.
+
+        Raises:
+          Timeout: If a timeout is given and it expires.
+
+        Parameters:
+          timeout: An optional timeout representing how long to wait
+            for the resource.
+        """
+        with self._cond:
+            while not self._pool:
+                if self._used_size != self._pool_size:
+                    self._used_size += 1
+                    return self._factory()
+
+                if not self._cond.wait(timeout):
+                    raise PoolTimeout()
+            return self._pool.pop()
+
+    def put(self, resource: ResourceT) -> None:
+        """Put a resource back.
+
+        Raises:
+          PoolFull: If the resource pool is full.
+        """
+        with self._cond:
+            if len(self._pool) == self._pool_size:
+                raise PoolFull()
+
+            self._pool.append(resource)
+            self._cond.notify()
+
+    def discard(self, resource: ResourceT) -> None:
+        """Discard a resource from the pool.
+        """
+        with self._cond:
+            self._used_size = max(0, self._used_size - 1)
+            self._cond.notify()
+
+    def __len__(self) -> int:
+        """Get the number of resources currently in the pool.
+        """
+        return len(self._pool)
--- test_resource_pool.py
+++ test_resource_pool.py
@@ -1,44 +1,50 @@
 import pytest
+import time
 
-from resource_pool import Pool, PoolTimeout, PoolFull
+from concurrent.futures import ThreadPoolExecutor, TimeoutError
+from resource_pool import LazyPool, Pool, PoolTimeout, PoolFull
 
 
-def test_can_reserve_elements_from_a_pool():
+@pytest.mark.parametrize("pool_class", [Pool, LazyPool])
+def test_can_reserve_elements_from_a_pool(pool_class):
     # Given that I have a resource pool
-    pool = Pool(lambda: {"test": 42}, pool_size=32)
+    pool = pool_class(lambda: {"test": 42}, pool_size=32)
 
     # When I try to reserve an element
     with pool.reserve() as d:
+        pool_size = len(pool)
+
         # Then I should get back a resource
         assert d == {"test": 42}
 
-        # And the current pool size should be one less
-        assert len(pool) == 31
-
     # When I exit the reserve block
     # Then the resource should get put back
-    assert len(pool) == 32
+    assert len(pool) == pool_size + 1
 
 
-def test_resources_get_put_back_on_error():
+@pytest.mark.parametrize("pool_class", [Pool, LazyPool])
+def test_resources_get_put_back_on_error(pool_class):
     # Given that I have a resource pool
-    pool = Pool(lambda: {"test": 42}, pool_size=32)
+    pool = pool_class(lambda: {"test": 42}, pool_size=32)
 
     # When I try to reserve an element
     try:
         with pool.reserve():
+            pool_size = len(pool)
+
             # And an unhandled exception occurs within the block
             raise Exception()
     except Exception:
         pass
 
-    # Then my resource should still be returned to the pool
-    assert len(pool) == 32
+    # Then my resource should be returned to the pool
+    assert len(pool) == pool_size + 1
 
 
-def test_get_can_timeout():
+@pytest.mark.parametrize("pool_class", [Pool, LazyPool])
+def test_get_can_timeout(pool_class):
     # Given that I have a resource pool of one element
-    pool = Pool(lambda: {}, pool_size=1)
+    pool = pool_class(lambda: {}, pool_size=1)
 
     # When I take that element out
     pool.get()
@@ -49,11 +55,145 @@ def test_get_can_timeout():
         pool.get(timeout=0.1)
 
 
-def test_pools_can_be_full():
+@pytest.mark.parametrize("pool_class", [Pool, LazyPool])
+def test_pools_can_be_full(pool_class):
     # Given that I have a resource pool of one element
-    pool = Pool(lambda: {}, pool_size=1)
+    pool = pool_class(lambda: {}, pool_size=1)
+
+    # And I've reserved and released one element so as to make sure the lazy pool instantiates it
+    with pool.reserve():
+        pass
 
     # When I try to put an element back even though it is full
     # Then a PoolFull should be raised
     with pytest.raises(PoolFull):
         pool.put({})
+
+
+def test_can_reserve_elements_from_a_lazy_pool():
+    # Given that I have a lazy resource pool with a factory that counts its number of instances
+    instances = 0
+
+    def factory():
+        nonlocal instances
+        instances += 1
+        return {"test": 42}
+
+    pool = LazyPool(factory, pool_size=8)
+
+    # When I try to reserve an element
+    with pool.reserve() as d:
+        # Then I should get back a resource
+        assert d == {"test": 42}
+
+        # And the instance count should be 1
+        assert instances == 1
+
+        # And the current pool size should be one less
+        assert len(pool) == instances - 1
+
+    # When I exit the reserve block
+    # Then the resource should get put back
+    assert len(pool) == 1
+
+    # When I try to reserve another element
+    with pool.reserve() as d:
+        # Then I should get back a resource
+        assert d == {"test": 42}
+
+        # And the instance count should still be 1
+        assert instances == 1
+
+        # And the current pool size should be one less
+        assert len(pool) == instances - 1
+
+
+def test_lazy_pools_can_preload_resources():
+    # Given that I have a lazy resource pool with a factory that counts its number of instances
+    instances = 0
+
+    def factory():
+        nonlocal instances
+        instances += 1
+        return {"test": 42}
+
+    pool = LazyPool(factory, pool_size=8, min_instances=2)
+
+    # When I try to reserve an element
+    with pool.reserve() as d:
+        # Then I should get back a resource
+        assert d == {"test": 42}
+
+        # And the instance count should be 2
+        assert instances == 2
+
+        # And the current pool size should be one less
+        assert len(pool) == instances - 1
+
+
+def test_lazy_pools_can_block():
+    # Given that I have a lazy resource pool with a factory that counts its number of instances
+    instances = 0
+
+    def factory():
+        nonlocal instances
+        instances += 1
+        return {"test": 42}
+
+    pool = LazyPool(factory, pool_size=4)
+
+    # When I try to reserve 32 resources concurrently
+    with ThreadPoolExecutor(max_workers=32) as e:
+        reserved = 0
+
+        def background_job():
+            nonlocal reserved
+            with pool.reserve(timeout=10):
+                reserved += 1
+                time.sleep(0.1)
+
+        tasks = [e.submit(background_job) for _ in range(32)]
+        for task in tasks:
+            task.result()
+
+    # Then the number of reserved instances should be 32
+    assert reserved == 32
+
+    # And the number of instances should be 4
+    assert instances == 4
+
+
+def test_lazy_pools_can_discard_resources():
+    # Given that I have a lazy resource pool with a factory that counts its number of instances
+    instances = 0
+
+    def factory():
+        nonlocal instances
+        instances += 1
+        return {"test": 42}
+
+    pool = LazyPool(factory, pool_size=1)
+
+    with ThreadPoolExecutor(max_workers=8) as e:
+        # When I try to reserve an element
+        # Then I should get back a resource
+        d1 = pool.get()
+
+        # And the instance count should be 1
+        assert instances == 1
+
+        # When I try to get another resource from the pool
+        future = e.submit(lambda: pool.get(timeout=10))
+
+        # Then the future should be blocked indefinitely
+        with pytest.raises(TimeoutError):
+            future.result(timeout=0.2)
+
+        # When I discard that resource
+        pool.discard(d1)
+
+        # Then the future should be unblocked
+        d2 = future.result()
+        assert d2
+        assert d2 == d1
+        assert d2 is not d1
